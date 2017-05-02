ee2210d6bfc070831d3f6da3ba69bf3c05f85b70
RenaissanceZwy <zhaowenyi96@163.com>
2017-05-02 16:14:17 +0800

    1 singer相关controller

--- music-web/src/main/java/com/music/web/controller/SingerController.java
+++ music-web/src/main/java/com/music/web/controller/SingerController.java
@@ -1,7 +1,16 @@
 package com.music.web.controller;
 
+import com.music.web.constant.JsonResult;
+import com.music.web.entity.Singer;
+import com.music.web.service.SingerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2017/5/1.
@@ -9,4 +18,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "singer")
 @Controller
 public class SingerController {
+
+    @Autowired
+    private SingerService singerService;
+
+    /**
+     * 新建歌手
+     * @param request
+     * @param model
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "add")
+    public JsonResult add(HttpServletRequest request, Model model, Singer singer){
+        if(StringUtils.isEmpty(singer.getName())){
+            return new JsonResult("201","歌手名称不能为空","");
+        }
+        if(!singerService.insertSinger(singer)){
+            return new JsonResult("201","新建歌手失败","");
+        }
+        return new JsonResult("200","新建歌手成功","");
+    }
+
+    /**
+     * 进行删除歌手
+     * @param request
+     * @param model
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "delete")
+    public JsonResult delete(HttpServletRequest request,Model model){
+        String id = request.getParameter("id");
+        if(StringUtils.isEmpty(id)){
+            return new JsonResult("201","参数获取失败","");
+        }
+        if(!singerService.deleteSinger(Integer.valueOf(id))){
+            return new JsonResult("201","删除歌手失败","");
+        }
+        return new JsonResult("200","删除歌手成功","");
+    }
+
+    /**
+     * 获取歌手信息
+     * @param request
+     * @param model
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "info")
+    public JsonResult info(HttpServletRequest request,Model model){
+        String id = request.getParameter("id");
+        if(StringUtils.isEmpty(id)){
+            return new JsonResult("201","参数获取失败","");
+        }
+        Singer singer = singerService.selectSinger(Integer.valueOf(id));
+        return new JsonResult("200","获取歌手成功",singer);
+    }
 }
